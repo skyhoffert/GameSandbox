91a90cb4f69ef5a255470a7cf4da3d0004f45c7e
skyhoffert <skwylarh@yahoo.com>
2020-11-20 10:40:35 -0500

    Initial commit of the Unity Project.

---
+++ Assets/Scripts/MyScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+    private Vector3 home;
+    private bool trav_to_target = true;
+    private int frames = 0;
+    private float freq = 1;
+
+    public Vector3 target = Vector3.zero;
+    public float dur = 1; // seconds
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        home = transform.position;
+        freq = 2*Mathf.PI / (dur*60f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // float ratio = ((float)frames) / ((float)frame_count);
+        // float v = 0.5f*Mathf.Sin(ratio*Mathf.PI/2) + 1;
+        float v = 0.5f*Mathf.Sin(freq*((float)frames)) + 0.5f;
+        
+        transform.position = Vector3.Lerp(home, target, v);
+
+        if (trav_to_target == true) {
+        } else {
+            //transform.position = Vector3.Lerp(target, home, v);
+        }
+        
+        frames++;
+        /*
+        if (frames > 4*frame_count) {
+            frames = 0;
+            trav_to_target = !trav_to_target;
+        }
+        */
+
+        transform.Rotate(1f,1f,Random.value,Space.Self);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # GameSandbox
 Sandbox for making a game.
+
+## Requirements
+This repository can be run with Unity 2020.1.0b1. It is possible that it will not work with other version of Unity.
+
+## Description
+As of Nov 20, 2020 there is simply a cube with a green texture that oscillates between two points. The first point is the position that the cube starts in and the second point is given as a public value for the Cube object under the MyScript component. The duration of the oscillation can also be modified in the component.
+
